6e72496e9e800d604cbba7c5c28935a624b61c77
Michael030906 <mdscool03@gmail.com>
2022-07-05 12:20:09 -0500

    added login

--- src/app/auth/auth.component.ts
+++ src/app/auth/auth.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { NgForm } from '@angular/forms';
-import { AuthService } from './auth.service';
+import { Observable } from 'rxjs';
+import { AuthResponseData, AuthService } from './auth.service';
 
 @Component({
   selector: 'app-auth',
@@ -26,20 +27,23 @@ export class AuthComponent implements OnInit {
     }
     const email = form.value.email;
     const password = form.value.password;
+    let authObs: Observable<AuthResponseData>
+
     this.isLoading = true;
     if(this.isLoginMode){
-
+     authObs =  this.AuthService.login(email, password)
     }else{
-      this.AuthService.signup(email, password).subscribe(resData =>{
-        console.log(resData)
-        this.isLoading = false
-      },
-      errorMessage => {
-        console.log(errorMessage)
-        this.error = errorMessage
-        this.isLoading = false;
-      });
+      authObs = this.AuthService.signup(email, password)
     }
+    authObs.subscribe(resData =>{
+      console.log(resData)
+      this.isLoading = false
+    },
+    errorMessage => {
+      console.log(errorMessage)
+      this.error = errorMessage
+      this.isLoading = false;
+    });
     form.reset();
   }
 }
