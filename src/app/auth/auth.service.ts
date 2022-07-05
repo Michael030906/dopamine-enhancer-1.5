c90622312c8f52333c286f626414b5cba050c36f
Michael030906 <mdscool03@gmail.com>
2022-07-05 12:48:42 -0500

    added more error handling

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -1,4 +1,4 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { catchError, throwError } from 'rxjs';
 
@@ -24,17 +24,7 @@ export class AuthService {
       password:password,
       returnSecureToken:true
     }
-    ).pipe(catchError(errorRes => {
-      let errorMessage = 'An unknown error occurred!'
-      if (!errorRes.error || !errorRes.error.error){
-        return throwError(errorMessage)
-      }
-      switch(errorRes.error.error.message) {
-        case 'EMAIL_EXISTS':
-          errorMessage = 'This email is already in use';
-      }
-      return throwError(errorMessage)
-     }));
+    ).pipe(catchError(this.handleError));
   }
   login(email:string, password:string){
      return this.http.post<AuthResponseData>('https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyB3tFzISgI9Uot4VpUuN39sKiGg6rIj4I0',
@@ -42,6 +32,23 @@ export class AuthService {
       email:email,
       password:password,
       returnSecureToken:true
-    })
+    }).pipe(catchError(this.handleError));
   }
+  private handleError(errorRes: HttpErrorResponse){
+    let errorMessage = 'An unknown error occurred!'
+    if (!errorRes.error || !errorRes.error.error){
+      return throwError(errorMessage)
+    }
+    switch(errorRes.error.error.message) {
+      case 'EMAIL_EXISTS':
+        errorMessage = 'This email is already in use';
+        break;
+      case 'EMAIL_NOT_FOUND':
+        errorMessage = 'Incorrect Password/Email';
+        break
+      case 'INVALID_PASSWORD':
+        errorMessage = "Incorrect Password/Email"
+    }
+    return throwError(errorMessage);
+   };
 }
