df9ba52b59743374ffd5f9607f9c25c95cf067bf
Michael030906 <mdscool03@gmail.com>
2022-07-13 10:29:53 -0500

    adding in remote storage

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,16 +1,24 @@
-import { Component, EventEmitter, OnInit, Output } from '@angular/core';
+import { Component, EventEmitter, OnDestroy, OnInit, Output } from '@angular/core';
+import { Subscriber, Subscription } from 'rxjs';
+import { AuthService } from '../auth/auth.service';
 
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
   styleUrls: ['./navbar.component.css']
 })
-export class NavbarComponent implements OnInit {
+export class NavbarComponent implements OnInit, OnDestroy {
+  isAuththenticated = false;
+  private userSub: Subscription;
 
-
-  constructor() { }
+  constructor(private authService: AuthService) { }
 
   ngOnInit(): void {
+    this.userSub = this.authService.user.subscribe(user => {
+      this.isAuththenticated = !!user;
+    })
   }
-
+  ngOnDestroy(): void {
+      this.userSub.unsubscribe()
+  }
 }
